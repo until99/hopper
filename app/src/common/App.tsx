fce0cf736a732e91e12d4babcb59947ee49ca72d
untill99 <gabriel.kasten@outlook.com>
2025-02-26 16:48:24 -0300

    Add Phosphor Icons dependency, restructure application components, and implement ReportPage

---
+++ app/src/common/App.tsx
@@ -0,0 +1,13 @@
+import Home from "../landing/Home";
+import ReportPage from "../report/ReportPage";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-slate-900">
+      {/* <Home /> */}
+      <ReportPage />
+    </div>
+  )
+}
+
+export default App;
--- app/src/landing/Home.tsx
+++ app/src/landing/Home.tsx
@@ -1,8 +1,22 @@
+import { Alien } from "@phosphor-icons/react";
+
 function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div>
+      {/* Navbar */}
+      <nav className="bg-blue-400 w-full h-18 mb-5"></nav>
+
+      {/* Cards */}
+      <ul>
+        <li className="bg-blue-400 w-[300px] h-[150px] mx-5 rounded cursor-pointer">
+          <a href="/report" className="flex flex-col justify-center items-center h-full">
+            <Alien size={32} />
+            <p>Homologação</p>
+          </a>
+        </li>
+      </ul>
+
+    </div >
   )
 }
 
