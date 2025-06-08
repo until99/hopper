aa8b82c370e8ee401be1a870d6dfe15ce50ac882
untill99 <gabriel.kasten@outlook.com>
2025-06-07 22:40:28 -0300

    chore: remove unused assets and components to clean up the project

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   interface QueryResult {
     columns: string[];
-    rows: { [key: string]: any }[];
+    rows: Array<{ [key: string]: any }>;
     total: number;
   }
 
@@ -20,11 +20,15 @@ export default function Home() {
   return (
     <>
       <main className="p-5 flex flex-col h-screen gap-8">
+        {/* <Textarea /> */}
         <textarea placeholder="SQL Query" className="w-full h-64 p-2 border border-gray-300 rounded mt-4"></textarea>
+
+        {/* <Button /> */}
         <div className="w-fit flex items-center justify-between px-8 py-2 gap-4 bg-green rounded-lg hover:cursor-pointer bg-blue-600 hover:bg-blue-700">
           <button className="text-white font-semibold hover:cursor-pointer">Execute</button>
         </div>
 
+        {/* <TableWithQueryResult /> */}
         <div className="rounded-lg bg-white overflow-hidden">
           <table className="w-full border-collapse border border-gray-200">
             <thead>
