acc1ca4c70e91be010b2eedb83a0331e1e71d343
untill99 <gabriel.kasten@outlook.com>
2025-08-19 03:40:30 -0300

    feat(home): ✨ implement new Home component layout
    * Added a new `Home` component with a sidebar and navigation links.
    * Integrated `@phosphor-icons/react` for icon usage.
    * Removed the old `navbar` component and associated references.
    * Updated `package.json` and `pnpm-lock.yaml` to include new dependencies.

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import Home from './pages/home.jsx'
+import Home from "./pages/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Home />
   </StrictMode>,
-)
+);
