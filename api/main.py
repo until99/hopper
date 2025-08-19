7c3c04f4dfef571c773891eb892383136287d41c
untill99 <gabriel.kasten@outlook.com>
2025-08-19 00:48:18 -0300

    chore: 🗑️ remove unused files and configurations
    * Deleted `.gitignore` entries for `.env` and various `__pycache__` files.
    * Removed obsolete Python files from `api/config.py`, `api/main.py`, `api/services/powerbi.py`, and others.
    * Cleans up the project structure by eliminating unnecessary code and files.

--- api/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from api.v1 import router as api_v1_router
-import logging
-
-
-logger = logging.getLogger("api")
-
-app = FastAPI()
-app.include_router(api_v1_router, prefix="/api/v1")
--- api/v1/__init__.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from .reports import router as dashboards_router
-
-router = APIRouter()
-router.include_router(dashboards_router, prefix="/dashboards", tags=["dashboards"])
